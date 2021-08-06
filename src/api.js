4de7c06ba7b628b1beb63401c2c1fdffb5b54ced
ankitsoni96 <ankitvsoni1996@gmail.com>
2021-08-07 00:52:10 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const serverless = require('serverless-http')
+const router = express.Router()
+
+
+router.get('/',(req,res)=>{
+    res.json({
+        message:"Working"
+    })
+})
+
+app.use('/.netlify/functions/api',router)
+
+
+module.exports.handler = serverless(app)
